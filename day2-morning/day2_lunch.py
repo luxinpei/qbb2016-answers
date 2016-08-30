a793a842144797a6228e0cd647cd82c288185fa1
Luxin Pei <lpei6@jhu.edu>
2016-08-30 17:00:58 -0400

    updated py

--- day2-morning/day2_lunch.py
+++ day2-morning/day2_lunch.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
-import sys
-f = open( sys.argv[1] )
-
-counter = 0
-for line in f:
-    if line.startswith( "SRR"):
-        counter +=1
-
-print counter
+# import sys
+# f = open( sys.argv[1] )
+#
+# counter = 0
+# for line in f:
+#     if line.startswith( "SRR"):
+#         counter +=1
+#
+# print counter
 
 # f = open( 'SRR072893.sam' )
 #
@@ -29,9 +29,9 @@ print counter
 # for line in f:
 #     if "NH:i:1" in line:
 #             counter +=1
+# #
+# # print counter
 #
-# print counter
-
 # import sys
 # f = open( sys.argv[1] )
 #
@@ -39,23 +39,23 @@ print counter
 #
 # for line in f:
 #     if line.startswith( "SRR"):
-#         print(lines.split("\t")[2])
+#         print(line.split("\t")[2])
 #         counter +=1
 #     if counter == 10:
 #         break
 #
-# import sys
-# f = open( sys.argv[1] )
-# total_value = 0
-# entry = 0.0
-#
-# for line in f:
-#     if line.startswith( "SRR"):
-#         if line.split("\t")[4] != "255":
-#            total_value += int(line.split("\t")[4])
-#            entry += 1
-#
-# print (total_value / entry )
+import sys
+f = open( sys.argv[1] )
+total_value = 0
+entry = 0.0
+
+for line in f:
+    if line.startswith( "SRR"):
+        if line.split("\t")[4] != "255":
+           total_value += int(line.split("\t")[4])
+           entry += 1
+
+print (total_value / entry )
 
 
 
